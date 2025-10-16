dd1af1db7997526677ff19d42a8c42c2b25162f5
SufiyanMohammed <sssufiyan98@gmail.com>
2025-10-16 18:22:41 +0530

    solved the errors sessions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 if (process.env.NODE_ENV != "Production") {
   require("dotenv").config();
 }
+
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(path.join(__dirname, "public")));
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const session = require("express-session");
@@ -13,25 +13,32 @@ const flash = require("connect-flash");
 const ejsMate = require("ejs-mate");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const { send } = require("process");
+const port = process.env.PORT || 8080;
+
+const Listing = require("./models/listing.js");
 const User = require("./models/user.js");
-app.engine("ejs", ejsMate);
-app.use(methodOverride("_method"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-const listing = require("./models/listing.js");
-const review = require("./models/review.js");
-const asyncwrap = require("./public/utils/wrapasync.js");
-const expresserror = require("./public/utils/expresserror.js");
-const wrapasync = require("./public/utils/wrapasync.js");
+const Review = require("./models/review.js");
+
+const wrapAsync = require("./public/utils/wrapasync.js");
+const ExpressError = require("./public/utils/expresserror.js");
 const { listingschema, reviewschema } = require("./schemajoi.js");
+
+
 const Routerlistings = require("./routes/listing.js");
 const Routerreviews = require("./routes/review.js");
 const Routeruser = require("./routes/user.js");
-const { send } = require("process");
-const port = process.env.PORT || 8080;
+
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 let dbAtlasurl = process.env.ATLAS_URL;
 
